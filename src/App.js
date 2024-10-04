c515b8062e8ef85e3a02e494f7cce53c064b7572
NahidKakon <nahidshakib@outlook.com>
2024-10-04 15:35:54 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import Accordian from "./components/accordian";
+import './App.css';
+
+function App() {
+  return (
+    <div classname="App">
+      {/* Accordian component */}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
